10d5d3f545b5b5b6612a00ae57bedff5781abdbe
vagne <vagne@its_Company>
2022-09-13 14:59:51 -0300

    versao 2

--- VagnerTest/src/Main.java
+++ VagnerTest/src/Main.java
@@ -2,7 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World 2");
+		System.out.println("Hello World 2");
 
 	}
 
